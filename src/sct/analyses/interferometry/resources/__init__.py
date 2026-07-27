e04a1426c6412e04a254fef654d86f4f6a158345
Giorgio Parma <giorgio.parma@aresys.it>
2026-07-27 18:01:23 +0200

    moving interferometry analysis implementation from sct source code to dedicated plugin

--- src/sct/analyses/interferometry/resources/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Interferometric Analysis resources"""
-
-from importlib import resources
-
-config_schema = resources.files(__package__).joinpath("config_schema.json")
