74465b7ab0c56ffd89a9bb6006ac875d9b257949
Giorgio Parma <giorgio.parma@aresys.it>
2026-07-27 11:08:21 +0200

    moving spectral analysis implementation from sct source code to dedicated plugin

--- src/sct/analyses/spectra/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Point Target and Distributed Spectral Analysis feature.
-
-Discovered by SCT via the ``sct.analyses`` entry-point namespace
-(see `sct.analyses.spectra.plugin.SpectralAnalysisPlugin`).
-"""
--- tests/unittest/analyses/spectra/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Spectral analysis unit tests"""
