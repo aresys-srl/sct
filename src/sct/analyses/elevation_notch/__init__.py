427cac370e6f0da422da40866a4234fc5fc561a9
Giorgio Parma <giorgio.parma@aresys.it>
2026-07-27 18:00:34 +0200

    moving notch analysis implementation from sct source code to dedicated plugin

--- src/sct/analyses/elevation_notch/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Elevation Notch Analysis feature.
-
-Discovered by SCT via the ``sct.analyses`` entry-point namespace
-(see `sct.analyses.elevation_notch.plugin.ElevationNotchAnalysisPlugin`).
-"""
