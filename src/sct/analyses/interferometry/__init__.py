3c7ada5c2f46d64eb256be04d9a67f19eac1fad1
Giorgio Parma <giorgio.parma@aresys.it>
2026-07-27 18:01:23 +0200

    moving interferometry analysis implementation from sct source code to dedicated plugin

--- src/sct/analyses/interferometry/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Interferometric Analysis feature.
-
-Discovered by SCT via the ``sct.analyses`` entry-point namespace
-(see `sct.analyses.interferometry.plugin.InterferometricAnalysisPlugin`).
-"""
--- src/sct/analyses/interferometry/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Interferometric Analysis - Core implementation"""
-
-from sct.analyses.interferometry.core.analysis import sct_interferometric_coherence_analysis
-
-__all__ = ["sct_interferometric_coherence_analysis"]
