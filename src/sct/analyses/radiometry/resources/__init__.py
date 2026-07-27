9727975a34a5580777f818490a05dc37b5ea8398
Giorgio Parma <giorgio.parma@aresys.it>
2026-07-27 17:57:22 +0200

    moving radiometric analysis implementation from sct source code to dedicated plugin

--- src/sct/analyses/radiometry/resources/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Radiometric Analysis resources"""
-
-from importlib import resources
-
-config_schema = resources.files(__package__).joinpath("config_schema.json")
--- tests/unittest/analyses/ra/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Radiometric analysis unit tests"""
